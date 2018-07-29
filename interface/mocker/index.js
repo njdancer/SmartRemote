a0e1d0411385247106451742349994280bc0924f
Nick Dancer <nick@dncr.me>
2018-07-29 20:02:35 +1000

    Mock current ac api on dev server

--- interface/mocker/index.js
+++ interface/mocker/index.js
@@ -1,9 +1,25 @@
+var acState = {
+  power: false,
+  mode: 0,
+  temp: 24,
+};
+
 const proxy = {
-  'GET /api/ac': {
-    power: true,
-    mode: 2,
-    temp: 28
-  }
+  'GET /api/ac': (req, res) => { return res.json(acState); },
+  'PATCH /api/ac': (req, res) => {
+    // Sanitise temp and mode
+    var mode = req.body.mode;
+    if (mode) mode = Math.max(0, Math.min(4, mode));
+
+    var temp = req.body.temp;
+    if (temp) temp = Math.max(16, Math.min(30, temp));
+
+    return res.json(Object.assign(acState, {
+      power: req.body.power,
+      mode,
+      temp,
+    }));
+  },
 };
 
 module.exports = proxy;
