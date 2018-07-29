3bc8bf10df8b3971619641bcde7d3b9bb74c787f
Nick Dancer <nick@dncr.me>
2018-07-29 18:54:17 +1000

    Fix issue where color prop was being passed to button
    Because color is passed to ModeButton it is automatically added to button. To avoid this you can use a wrapper functional component and object destructuring to remove the color prop whilst keeping it accessible to template literal. Unfortunately if this is done directly as a parameter to styled it shows as an unknown component in React inspector. Assigning the wrapper to a constant and passing to styled resolves this.

--- interface/src/ModeButton.js
+++ interface/src/ModeButton.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import styled from 'styled-components';
 
-export default styled.button`
+const StyledButton = ({ color, ...rest }) => {
+  return <button {...rest} />
+}
+
+export default styled(StyledButton)`
   border-radius: 8px;
   border:1px solid black;
   cursor:pointer;
-  width: 40px;
-  height: 40px;
+  width: 60px;
+  height: 60px;
   transition: background-color 0.1s, color 0.1s, transform 0.1s;
   border-color: ${({color}) => color || 'black'};
   background-color: ${({active, color}) => active ? color : 'transparent'};
